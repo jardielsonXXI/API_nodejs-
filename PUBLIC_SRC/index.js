39cbbf2b5070b150aafbb760f088bfdc7079ef90
Jardielson s <59547529+jardielsonXXI@users.noreply.github.com>
2020-06-29 13:47:23 -0300

    Update index.js
    Call directory of create user to file main

--- PUBLIC_SRC/index.js
+++ PUBLIC_SRC/index.js
@@ -3,7 +3,7 @@ const app=express();
 const bodyparse=require('body-parser');
 app.use(bodyparser.json());
 app.use(bodyParser.urlencoded({extend:false}));
-
+require('./controllers/authecontrollers')(app);
 
 
 app.listen(8081);
